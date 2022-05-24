081dd456c3499903ba0bd78eab3ca5e19b7dae49
Nur Nechushtan <495554@student.fontys.nl>
2022-05-24 13:54:06 +0200

    UML diagram

---
+++ Logic/BadmintonResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class BadmintonResult : IRule
+    {
+        public bool ShouldRun(int resultOne, int resultTwo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunRule(int resultOne, int resultTwo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Logic/IRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public interface IRule
+    {
+        bool ShouldRun(int resultOne, int resultTwo);
+        void RunRule(int resultOne, int resultTwo);
+    }
+}
